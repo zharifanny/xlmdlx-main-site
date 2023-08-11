a4f8c65a76578ab253de90fb9a2510906fe80ee5
zharifanny <zharifanny@gmail.com>
2023-08-12 03:30:53 +0700

    remove zoom script from req.js

--- src/req.js
+++ src/req.js
@@ -2,9 +2,3 @@ document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
   });
 
-
-  $(document).ready(function() {
-    $(".zoomable").on("click", function() {
-        $(this).toggleClass("zoomed");
-    });
-});
