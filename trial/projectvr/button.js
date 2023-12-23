ec919bba3ebe1905433b72b9696cc002c2eaf323
Khindy Zharifany <zharifanny@gmail.com>
2023-12-23 23:12:22 +0700

    commit

--- trial/projectvr/button.js
+++ trial/projectvr/button.js
@@ -1,14 +1,14 @@
 // Simulating 'W' key press in VR
 document.getElementById('vrButton').addEventListener('click', function() {
-    // Your code to simulate 'W' key press in Three.js VR environment
-    // For example, if you're controlling a VR object's movement with 'W':
-    simulateWKeyPress();
-  });
+  // Your code to simulate 'W' key press in Three.js VR environment
+  // For example, if you're controlling a VR object's movement with 'W':
+  simulateWKeyPress();
+});
 
-  function simulateWKeyPress() {
-    // This function should trigger the desired action in your Three.js VR environment
-    // For example, moving the VR camera or an object forward
-    // Here's a placeholder function
-    console.log("Simulating 'W' key press in Three.js VR");
-    // Add your Three.js VR logic here to move forward
-  }
+function simulateWKeyPress() {
+  // This function should trigger the desired action in your Three.js VR environment
+  // For example, moving the VR camera or an object forward
+  // Here's a placeholder function
+  console.log("Simulating 'W' key press in Three.js VR");
+  // Add your Three.js VR logic here to move forward
+}
--- trial/projectvr/kibor.js
+++ trial/projectvr/kibor.js
@@ -1,4 +1,3 @@
 canvas.addEventListener("click", async () => {
-    await canvas.requestPointerLock();
-  });
-
+  await canvas.requestPointerLock();
+});
