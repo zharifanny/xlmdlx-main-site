cd86f65bb86ac3b55e65344368b42fab733335e3
Khindy Zharifany <zharifanny@gmail.com>
2024-12-29 16:20:47 +0700

    slot 5

--- slot/global.d.ts
+++
@@ -1,9 +0,0 @@
-import Game from "./core/Game";
-
-declare global {
-    interface Window {
-        game: Game;
-    }
-}
-
-export {};
--- slot/index.ts
+++
@@ -1,5 +0,0 @@
-import Game from "./core/Game";
-
-const game = new Game();
-game.init();
-window.game = game;
